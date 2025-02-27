f0dd370ee5d4928e417718230fff7c4ed5af4af9
ECspezi <conwecs@gmail.com>
2025-02-27 19:20:10 +0400

    front

--- calculator/front.lua
+++ calculator/front.lua
@@ -1,3 +1,22 @@
 local basalt = require("basalt")
 
 local main = basalt.createFrame()
+    :setBackground(colors.black)
+
+local button1 = main:addButton()
+    :setText("1")
+    :setSize(5, 3)
+    :setBackground(colors.gray)
+    :setPosition(2, 6)
+
+local button2 = main:addButton()
+    :setText("1")
+    :setSize(5, 3)
+    :setBackground(colors.gray)
+    :setPosition(7, 6)
+
+local button3 = main:addButton()
+    :setText("1")
+    :setSize(5, 3)
+    :setBackground(colors.gray)
+    :setPosition(12, 6)
