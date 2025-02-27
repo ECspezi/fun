934913d375f786667832566f17294d79091a54db
ECspezi <conwecs@gmail.com>
2025-02-28 03:04:32 +0400

    init2

--- calcremade/calc.lua
+++ calcremade/calc.lua
@@ -19,18 +19,21 @@ local function clearCalculator()
     lastNumber = 0
 end
 
+local startX, startY = 2, 8
 for i = 1, 9 do
     local startX, startY = 2, 8
     if i >= 3 and i < 6 then startY = 12 end
     if i >= 6 then startY = 16 end
-    main:addButton()
+    main:addButton("numberButton" .. i)
         :setText(tostring(i))
         :setSize(5, 3)
         :setPosition(startX, startY)
         :setText(tostring(i))
-        :onClick(function(button)
+        :onClick(function()
             basalt.debug("Button " .. i .. " clicked")
         end)
     startX = startX + 5
     if startX > 14 then startX = 2 end
 end
+
+basalt.autoUpdate()
