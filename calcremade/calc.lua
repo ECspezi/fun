506698d2b4a621630ab1cfd6a5c7f998000289bc
ECspezi <conwecs@gmail.com>
2025-02-28 02:48:26 +0400

    calc remake init

---
+++ calcremade/calc.lua
@@ -0,0 +1,36 @@
+local basalt = require("basalt")
+
+basalt.setTheme({
+    BaseFrameBG = colors.black,
+    ButtonBG = colors.gray,
+    ButtonText = colors.white,
+    LabelText = colors.white
+})
+
+local main = basalt.createFrame()
+
+local input = ""
+local lastOperation = ""
+local lastNumber = 0
+
+local function clearCalculator()
+    input = ""
+    lastOperation = ""
+    lastNumber = 0
+end
+
+for i = 1, 9 do
+    local startX, startY = 2, 8
+    if i >= 3 and i < 6 then startY = 12 end
+    if i >= 6 then startY = 16 end
+    main:addButton()
+        :setText(tostring(i))
+        :setSize(5, 3)
+        :setPosition(startX, startY)
+        :setText(tostring(i))
+        :onClick(function(button)
+            basalt.debug("Button " .. i .. " clicked")
+        end)
+    startX = startX + 5
+    if startX > 14 then startX = 2 end
+end
---
+++ test.lua
@@ -0,0 +1,7 @@
+-- local value = nil
+
+repeat
+    print(value)
+    value = 1
+    print(value)
+until value == 1
